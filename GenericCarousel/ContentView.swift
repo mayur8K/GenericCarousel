0776c585595531a3c3fd7665876fdb87cd346185
Mayur Kamthe <mayursmac@Mayurs-MacBook-Pro.local>
2025-04-14 15:26:07 +0530

    logic without using frames

--- GenericCarousel/ContentView.swift
+++ GenericCarousel/ContentView.swift
@@ -17,9 +17,11 @@ struct ContentView: View {
     @ViewBuilder
     internal var loginViewCarousel: some View {
         //here we have sent data to carousel, in future we can send model class data to the carousel.
-        CarouselView(imagesArray: ["1", "2", "3", "4", "5", "6", "7"],
+        let imagesArray = ["1", "2", "3", "4", "5", "6", "7"]
+        CarouselView(list: imagesArray,
                      width: 250,
                      height: 250) {
+           
         }
     }
 }
@@ -32,59 +34,63 @@ struct CarouselView<Content: View>: View {
     var pageHeight: CGFloat
     @State private var scrollPosition: Int?
     private let animation: Animation = .default
-    let imagesArray: [String]
     let content: Content
+    let list: [String]
+    private let itemDimension = UIScreen.main.bounds.width
+    private let itemSpacing = CGFloat(16)
+    private let scrollViewHorizontalInset = CGFloat(8)
+
     
-    init(imagesArray: [String], width: CGFloat, height: CGFloat, @ViewBuilder content: () -> Content) {
+    init(list: [String], width: CGFloat, height: CGFloat, @ViewBuilder content: () -> Content) {
         self.pageWidth = width
         self.pageHeight = height
-        self.imagesArray = imagesArray
+        self.list = list
         self.content = content()
     }
     
     var body: some View {
-        let dynamicWidth = UIScreen.main.bounds.width - pageWidth
         VStack {
             ScrollView(.horizontal) {
+               // GeometryReader { proxy in
                 HStack {
-                        ForEach(0..<imagesArray.count, id: \.self) { index in
-                            Image(imagesArray[index])
-                                   .resizable()
-                                   .aspectRatio(contentMode: .fill)
-                                   .frame(width: pageWidth, height: pageHeight)
-                                   .cornerRadius(20)
-                                   .scrollTransition{ content, phase in
-                                       content
-                                           .opacity(phase.isIdentity ? 1 : 0.7) // Apply opacity animation
-                                           .scaleEffect(y: phase.isIdentity ? 1 : 0.8)
-                                   }
+                    ForEach(0..<list.count, id: \.self) { index in
+                        Image(list[index])
+                            .resizable()
+                            .aspectRatio(40/40, contentMode: .fill)
+                            .cornerRadius(10)
+                            .scrollTransition{ content, phase in
+                                content
+                                    .opacity(phase.isIdentity ? 1 : 0.7) // Apply opacity animation
+                                    .scaleEffect(y: phase.isIdentity ? 1 : 0.8)
+                            }
+                            .offset(x: (pageWidth/2 - itemDimension)/2 - (itemSpacing * CGFloat(list.count - 1)/2))
+
                     }
                 }
                 .scrollTargetLayout()
+                //}
             }
             .frame(height: pageHeight)
-            .contentMargins(dynamicWidth/2, for: .scrollContent)
             .scrollTargetBehavior(.viewAligned)
             .scrollPosition(id: $scrollPosition, anchor: .center)
             .scrollIndicators(.hidden)
             .onChange(of: scrollPosition) {
                 guard let scrollPosition = scrollPosition else {return}
                 print(scrollPosition)
+                
             }
 
             HStack {
-                ForEach(0..<imagesArray.count, id: \.self) { index in
+                ForEach(0..<list.count, id: \.self) { index in
                     Circle()
                         .fill(Color.gray.opacity(
-                            (index == (scrollPosition ?? 0) % imagesArray.count) ? 0.8 : 0.3
+                            (index == (scrollPosition ?? 0) % list.count) ? 0.8 : 0.3
                         ))
                         .frame(width: 9)
                 }
             }
         }
     }
-    
-    
 }
 
 #Preview {
